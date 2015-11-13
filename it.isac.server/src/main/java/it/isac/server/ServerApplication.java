0c324d9ff65118cadaada40ac41b9b294b7844f9
pievis <pierluigi.montagna220@gmail.com>
2015-11-13 17:21:47 +0100

    added an html visualizer

--- it.isac.server/src/main/java/it/isac/server/ServerApplication.java
+++ it.isac.server/src/main/java/it/isac/server/ServerApplication.java
@@ -1,6 +1,7 @@
 package it.isac.server;
 
 import it.isac.server.resources.NeighboursServerResource;
+import it.isac.server.resources.NetworkServerResource;
 import it.isac.server.resources.NodeServerResource;
 import it.isac.server.resources.NodesServerResource;
 import it.isac.server.resources.RootServerResource;
@@ -17,17 +18,19 @@ import org.restlet.routing.Router;
 
 /**
  * This is a simple restlet application used for the spatial computing server.
+ * 
  * @author Pievis
  */
 public class ServerApplication extends Application {
-	
+
 	final static String BASE_URL = "/";
-	final static Logger LOGGER = Logger.getLogger(ServerApplication.class.getName());
-	
+	final static Logger LOGGER = Logger.getLogger(ServerApplication.class
+			.getName());
+
 	@Override
-	public Restlet createInboundRoot(){
+	public Restlet createInboundRoot() {
 		Router router = new Router(getContext());
-		//Node Resource
+		// Node Resource
 		router.attach(BASE_URL, RootServerResource.class);
 		router.attach(BASE_URL + UrlAttributes.getRelNodeUrl(),
 				NodeServerResource.class);
@@ -35,22 +38,25 @@ public class ServerApplication extends Application {
 				NodesServerResource.class);
 		router.attach(BASE_URL + UrlAttributes.getRelNodeNbrUrl(),
 				NeighboursServerResource.class);
-		//TODO add other resources if necessary
-		return router; 
+		router.attach(BASE_URL + UrlAttributes.getRelNetUrl(),
+				NetworkServerResource.class);
+		// TODO add other resources if necessary
+		return router;
 	}
-	
-	public ServerApplication(){
+
+	public ServerApplication() {
 		setName("Spatial Computing Mediator");
 		setAuthor("Pierluigi Montagna");
 	}
-	
-	void configureLogger() throws SecurityException, IOException{
+
+	void configureLogger() throws SecurityException, IOException {
 		ServerConfig.generateLoggerDir();
-//		String dir = ServerConfig.getLoggerFilesFolder();
-//		LOGGER.info("Setted logger dir: " + dir);
-//		//Set a global file handler for the logger
-//		FileHandler fileHandler = new FileHandler(dir + "logfile%g.log", 5242880, 5, true);
-//		fileHandler.setLevel(Level.ALL);
-//		Logger.getGlobal().addHandler(fileHandler);
+		// String dir = ServerConfig.getLoggerFilesFolder();
+		// LOGGER.info("Setted logger dir: " + dir);
+		// //Set a global file handler for the logger
+		// FileHandler fileHandler = new FileHandler(dir + "logfile%g.log",
+		// 5242880, 5, true);
+		// fileHandler.setLevel(Level.ALL);
+		// Logger.getGlobal().addHandler(fileHandler);
 	}
 }
