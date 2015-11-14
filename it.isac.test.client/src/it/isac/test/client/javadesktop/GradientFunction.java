cee7256a4cbb53a7583c73a793ad2750a0c061a1
pievis <pierluigi.montagna220@gmail.com>
2015-11-14 16:48:17 +0100

    .

--- it.isac.test.client/src/it/isac/test/client/javadesktop/GradientFunction.java
+++ it.isac.test.client/src/it/isac/test/client/javadesktop/GradientFunction.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.naming.NotContextException;
 
+import it.isac.client.impl.device.Domain;
 import it.isac.client.impl.device.ExecutionContext;
 import it.isac.client.impl.device.FieldCalculusFunction;
 import it.isac.commons.interfaces.INodeValue;
@@ -25,11 +26,21 @@ public class GradientFunction extends FieldCalculusFunction {
 		ISensorSnapshot source = null;
 		// find the source sensor
 		for (String k : localSensors.keySet()) {
-			if (localSensors.get(k).getSensorId() == "source")
-				;
-			source = localSensors.get(k);
+			if (localSensors.get(k) != null) {
+				if (localSensors.get(k).getSensorId() == "source")
+					source = localSensors.get(k);
+			} else {
+				nextValue.setValue("Error");
+			}
 		}
 		// is source
+		ExecutionContext<?> exec = null;
+		try {
+			exec = getExecutionContext();
+		} catch (NotContextException e) {
+			nextValue.setValue("Error");
+			e.printStackTrace();
+		}
 		if (Boolean.parseBoolean(source.getValue())) {
 			nextValue.setValue("0"); // distance 0
 		} else { // isn't source
@@ -37,29 +48,25 @@ public class GradientFunction extends FieldCalculusFunction {
 			double minDist = Double.MAX_VALUE;
 			// find the node with min distance
 			for (String k : nbrState.keySet()) {
-				NodeState ns = nbrState.get(k);
-				double dist = Double.MAX_VALUE;
-				for (INodeValue v : ns.getValues()) {
-					if (v.getKey() == "distGrad") // find the correct value
-						dist = Double.parseDouble(v.getValue());
-				}
-				if (dist < minDist) {
-					minDist = dist;
-					keyMinNbr = k;
+				if (k != exec.getNodeId()) {
+					NodeState ns = nbrState.get(k);
+					double dist = Double.MAX_VALUE;
+					for (INodeValue v : ns.getValues()) {
+						if (v.getKey() == "distGrad") // find the correct value
+							dist = Double.parseDouble(v.getValue());
+					}
+					if (dist < minDist) {
+						minDist = dist;
+						keyMinNbr = k;
+					}
 				}
 			}
 			if (keyMinNbr.isEmpty())
 				nextValue.setValue("inf");
 			else {
-				ExecutionContext<?> exec;
-				try {
-					exec = getExecutionContext();
+				if (exec != null)
 					nextValue.setValue(minDist
 							+ Double.toString(exec.distanceTo(keyMinNbr)));
-				} catch (NotContextException e) {
-					nextValue.setValue("Error");
-					e.printStackTrace();
-				}
 			}
 		}
 		return nextValue;
